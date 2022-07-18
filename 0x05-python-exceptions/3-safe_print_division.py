5f0fe672dcb3076fff32e72c04d02ceea0a1b405
hailemariam <hailemariam37@gmail.com>
2022-07-18 02:32:35 -0700

    a function that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    div = 0
-    try:
-        div = a / b
-        return div
-    except:
-        div = None
-        return None
-    finally:
-        print("Inside result: {}".format(div))
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return (result)
