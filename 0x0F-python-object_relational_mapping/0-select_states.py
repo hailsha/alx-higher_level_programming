894b6f0c70204c9ccac1fb596ec8ade60af67e26
hailemariam <hailemariam37@gmail.com>
2022-10-17 01:09:35 -0700

    a script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-""" Importing MySQLdb for database tasks. Sys is for retrieving
-    command line arguments """
-import MySQLdb
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 import sys
-""" 0-select_states.py """
-
-
-def getStates():
-    """
-    Driving function. Actually not necessary, but done to make
-    the entire code nicer to read.
-    Accesses the database with user and password, all passed in,
-    to localhost at port 3306. Then, retrieve all information
-    from the states database and prints them all out
-    """
-    mydb = MySQLdb.connect(host='localhost',
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           port=3306)
-
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM states ORDER BY states.id")
-    myresult = mycursor.fetchall()
-
-    for x in myresult:
-        print(x)
+import MySQLdb
 
 if __name__ == '__main__':
-    getStates()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
